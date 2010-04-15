cf0cbcbef9d184595096e0b73e3605724f99b289
Tomasz Stachewicz <tomekrs@o2.pl>
2010-04-15 18:23:35 +0200

    code written on 4th meeting

--- todo/app/controllers/tasks_controller.rb
+++ todo/app/controllers/tasks_controller.rb
@@ -4,8 +4,11 @@ class TasksController < ApplicationController
   end
 
   def create
-    task = Task.new(params[:task])
-    task.save
+    @task = Task.new(params[:task])
+    @task.save!
     redirect_to({:action => :index})
+  rescue
+    @tasks = Task.all
+    render :action => :index
   end
 end
--- todo/app/models/task.rb
+++ todo/app/models/task.rb
@@ -1,3 +1,14 @@
 class Task < ActiveRecord::Base
   belongs_to :priority
+
+  validates_presence_of :title
+  validates_length_of :title, :minimum => 5
+  validates_presence_of :description
+
+  before_save :append_some_text
+
+
+  def append_some_text
+    self.description += " ...ale urwal!"
+  end
 end
